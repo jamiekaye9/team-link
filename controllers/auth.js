1c9c1b2ed0f45a4084b26d2601caa4f86b305679
jamiekaye <jamie.kaye4@googlemail.com>
2025-02-08 11:45:46 +0000

    sign out complete - create/join a company complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const User = require('../models/user.js')
 const bcrypt = require('bcrypt')
 
+const runQueries = async () => {
+    await createCompany()
+}
+
 router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs')
 })
@@ -18,7 +22,11 @@ router.post('/sign-up', async (req, res) => {
     const hashedPassword = bcrypt.hashSync(req.body.password, 10)
     req.body.password = hashedPassword
     const user = await User.create(req.body)
-    res.send(`Thank you for signing up ${user.firstName}`)
+    req.session.user = {
+        username: user.username,
+        _id: user._id
+    }
+    res.render('auth/company.ejs')
 })
 
 router.get('/sign-in', (req, res) => {
@@ -42,6 +50,65 @@ router.post('/sign-in', async (req, res) => {
         _id: userInDatabase._id
     }
     res.redirect('/')
+})
+
+router.get('/sign-out', (req, res) => {
+    req.session.destroy()
+    res.redirect('/')
+})
+
+router.get('/company', (req, res) => {
+    res.render('auth/company.ejs')
+})
+
+router.post('/create-company', async (req, res) => {
+    const companyInDatabase = await User.findOne({ companyName: req.body.companyName })
+    if (companyInDatabase) {
+        return res.send('Company name has been taken')
+    }
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send('Password and Confirm Password must match')
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hashedPassword
+    const userId = req.session.user._id
+        const user = await User.findById(userId)
+        const companyData = {
+            companyName: req.body.companyName,
+            password: req.body.password
+        }
+        user.company.push(companyData)
+        await user.save()
+    res.send('Request to create a company received')
+})
+
+router.post('/join-company', async (req, res) => {
+    const companyInDatabase = await User.findOne({
+        company: { $elemMatch: { companyName: req.body.companyName }}
+    })
+    if (!companyInDatabase) {
+        return res.send('Company does not exist')
+    }
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        companyInDatabase.password
+    )
+    if (!validPassword) {
+        return res.send('Password incorrect')
+    }
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hashedPassword
+
+    const userId = req.session.user._id
+        const user = await User.findById(userId)
+        const companyData = {
+            companyName: req.body.companyName,
+            password: req.body.password
+        }
+        user.company.push(companyData)
+        await user.save()
+    res.send('You have joined the company')
 })
 
 module.exports = router
--- models/user.js
+++ models/user.js
@@ -1,5 +1,47 @@
 const mongoose = require('mongoose')
 
+const workerSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    team: {
+        type: String,
+        required: true
+    },
+    manager: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: Number,
+        required: true
+    },
+    isManager: {
+        type: Boolean
+    }
+})
+
+const companySchema = new mongoose.Schema({
+    companyName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    workers: [workerSchema]
+})
+
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -16,7 +58,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    company: [companySchema]
 })
 
 const User = mongoose.model('User', userSchema)
